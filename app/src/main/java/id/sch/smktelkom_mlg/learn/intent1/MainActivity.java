903f9a3f36bccfde13502abff763c99049f43d6b
Aura Lintang <auralintang77@gmail.com>
2016-10-09 13:42:05 +0700

    onBackPressed Part 1

--- app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/intent1/MainActivity.java
@@ -1,5 +1,6 @@
 package id.sch.smktelkom_mlg.learn.intent1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -16,7 +17,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v)
             {
-
+                startActivity(new Intent(MainActivity.this, HasilActivity.class));
             }
         });
     }
